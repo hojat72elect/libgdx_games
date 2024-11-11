41aaba0c3c05a9b8795f0744ced94ed69fb3bd3e
hojat72elect <hojat72elect@gmail.com>
2024-11-10 19:29:32 -0500

    Migrated desktop module to kotlin.

---
+++ libgdx_sokoban/desktop/src/com/nopalsoft/sokoban/desktop/DesktopLauncher.kt
@@ -0,0 +1,12 @@
+package com.nopalsoft.sokoban.desktop
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.sokoban.MainSokoban
+
+fun main(args: Array<String>) {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setTitle("Sokoban")
+    config.setWindowedMode(800, 480)
+    Lwjgl3Application(MainSokoban(), config)
+}
