4f4592197d89bd66d59f601893551347c1b2c401
hojat <hojat72elect@gmail.com>
2025-01-08 21:41:09 -0500

    docs

--- Klooni1010/core/src/dev/lonami/klooni/game/Scorer.java
+++ Klooni1010/core/src/dev/lonami/klooni/game/Scorer.java
@@ -6,21 +6,22 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import dev.lonami.klooni.serializer.BinSerializable;
 
-// Used to keep track of the current and maximum
-// score, and to also display it on the screen.
-// The maximum score is NOT saved automatically.
+
+
+/**
+ * Used to keep track of the current and maximum score of the player, and to also display it on the screen.
+ * The maximum score is NOT saved automatically.
+ */
 public class Scorer extends BaseScorer implements BinSerializable {
 
 
     private int highScore;
-    //region Constructor
 
     // The board size is required when calculating the score
     public Scorer(final Klooni game, GameLayout layout) {
         super(game, layout, Klooni.getMaxScore());
         highScore = Klooni.getMaxScore();
     }
-    //region Public methods
 
     public void saveScore() {
         if (isNewRecord()) {
@@ -37,11 +38,9 @@ public class Scorer extends BaseScorer implements BinSerializable {
     public boolean isGameOver() {
         return false;
     }
-    //region Serialization
 
     @Override
     public void write(DataOutputStream outputStream) throws IOException {
-        // currentScore, highScore
         outputStream.writeInt(currentScore);
         outputStream.writeInt(highScore);
     }
