02b8c7a3b9a9fa30f83d1a65b2b3eda00709a6a8
hojat <hojat72elect@gmail.com>
2025-05-29 01:36:13 -0400

    Cleaned android module as well.
    regarding issue #6

--- libgx_shark_adventure/android/src/com/nopalsoft/sharkadventure/AndroidLauncher.java
+++ libgx_shark_adventure/android/src/com/nopalsoft/sharkadventure/AndroidLauncher.java
@@ -2,7 +2,6 @@ package com.nopalsoft.sharkadventure;
 
 import android.os.Bundle;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -14,24 +13,4 @@ public class AndroidLauncher extends AndroidApplication {
         initialize(new MainShark(), config);
     }
 
-    @Override
-    public void showInterstitial() {
-
-    }
-
-    @Override
-    public void shareOnTwitter(String mensaje) {
-        String tweetUrl = "https://twitter.com/intent/tweet?text=" + mensaje + " Download it from &url=https://play.google.com/&hashtags=SharkAdventure";
-        Gdx.net.openURI(tweetUrl);
-    }
-
-    @Override
-    public void showAdBanner() {
-
-    }
-
-    @Override
-    public void hideAdBanner() {
-
-    }
 }
