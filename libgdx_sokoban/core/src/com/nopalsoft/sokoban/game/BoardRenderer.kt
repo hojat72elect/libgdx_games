a4ff07f7d0485f592a64473ad75238e87c43b5ec
hojat <hojat72elect@gmail.com>
2025-05-31 17:18:40 -0400

    BoardRenderer.java -> kt
    Regarding issue #1

---
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/game/BoardRenderer.kt
@@ -0,0 +1,28 @@
+package com.nopalsoft.sokoban.game
+
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.nopalsoft.sokoban.Assets
+import com.nopalsoft.sokoban.screens.Screens
+
+class BoardRenderer(val batch: SpriteBatch) {
+
+    private val camera = OrthographicCamera(Screens.SCREEN_WIDTH.toFloat(), Screens.SCREEN_HEIGHT.toFloat())
+    private val renderer = OrthogonalTiledMapRenderer(Assets.map, GameBoard.UNIT_SCALE)
+    private val tileLayer = renderer.getMap().getLayers().get("StaticMap") as TiledMapTileLayer?
+
+    init {
+        camera.position.set(Screens.SCREEN_WIDTH / 2F, Screens.SCREEN_HEIGHT / 2F, 0F)
+
+    }
+
+    fun render() {
+        camera.update()
+        renderer.setView(camera)
+        renderer.batch.begin()
+        renderer.renderTileLayer(tileLayer)
+        renderer.batch.end()
+    }
+}
