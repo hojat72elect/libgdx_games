795357a5c47c755f3143db563dfc79a0646a8e6f
hojat <hojat72elect@gmail.com>
2025-05-31 15:44:04 -0400

    Box.java -> kt
    Regarding issue #1

--- libgdx_sokoban/core/src/com/nopalsoft/sokoban/objects/Box.kt
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/objects/Box.kt
@@ -1,132 +1,84 @@
-package com.nopalsoft.sokoban.objects;
+package com.nopalsoft.sokoban.objects
 
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-import com.nopalsoft.sokoban.Assets;
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion
+import com.nopalsoft.sokoban.Assets
+import kotlin.math.abs
 
-public class Box extends Tiles {
-    public static final int COLOR_BEIGE = 1;
-    public static final int COLOR_DARK_BEIGE = -1;
-    public static final int COLOR_BLACK = 2;
-    public static final int COLOR_DARK_BLACK = -2;
-    public static final int COLOR_BLUE = 3;
-    public static final int COLOR_DARK_BLUE = -3;
-    public static final int COLOR_BROWN = 4;
-    public static final int COLOR_DARK_BROWN = -4;
-    public static final int COLOR_GRAY = 5;
-    public static final int COLOR_DARK_GRAY = -5;
-    public static final int COLOR_RED = 6;
-    public static final int COLOR_DARK_RED = -6;
-    public static final int COLOR_YELLOW = 7;
-    public static final int COLOR_DARK_YELLOW = -7;
-    public static final int COLOR_PURPLE = 8;
-    public static final int COLOR_DARK_PURPLE = -8;
-    int numColor;
+class Box(posicion: Int, color: String) : Tiles(posicion) {
 
-    public boolean isInRightEndPoint;
+    private var numColor = 0
 
-    AtlasRegion keyFrame;
+    @JvmField
+    var isInRightEndPoint = false
+    private var keyFrame: AtlasRegion? = null
 
-    public Box(int posicion, String color) {
-        super(posicion);
-
-        isInRightEndPoint = false;
-
-        switch (color) {
-            case "brown":
-                numColor = COLOR_BROWN;
-                break;
-            case "gray":
-                numColor = COLOR_GRAY;
-                break;
-            case "purple":
-                numColor = COLOR_PURPLE;
-                break;
-            case "blue":
-                numColor = COLOR_BLUE;
-                break;
-            case "black":
-                numColor = COLOR_BLACK;
-                break;
-            case "beige":
-                numColor = COLOR_BEIGE;
-                break;
-            case "yellow":
-                numColor = COLOR_YELLOW;
-                break;
-            case "red":
-                numColor = COLOR_RED;
-                break;
+    init {
+        when (color) {
+            "brown" -> numColor = COLOR_BROWN
+            "gray" -> numColor = COLOR_GRAY
+            "purple" -> numColor = COLOR_PURPLE
+            "blue" -> numColor = COLOR_BLUE
+            "black" -> numColor = COLOR_BLACK
+            "beige" -> numColor = COLOR_BEIGE
+            "yellow" -> numColor = COLOR_YELLOW
+            "red" -> numColor = COLOR_RED
         }
 
-        setTextureColor(numColor);
+        setTextureColor(numColor)
     }
 
-    private void setTextureColor(int numColor) {
-        switch (numColor) {
-            case COLOR_BEIGE:
-                keyFrame = Assets.beigeBox;
-                break;
-            case COLOR_DARK_BEIGE:
-                keyFrame = Assets.darkBeigeBox;
-                break;
-            case COLOR_BLACK:
-                keyFrame = Assets.blackBox;
-                break;
-            case COLOR_DARK_BLACK:
-                keyFrame = Assets.darkBlackBox;
-                break;
-            case COLOR_BLUE:
-                keyFrame = Assets.blueBox;
-                break;
-            case COLOR_DARK_BLUE:
-                keyFrame = Assets.darkBlueBox;
-                break;
-            case COLOR_BROWN:
-                keyFrame = Assets.brownBox;
-                break;
-            case COLOR_DARK_BROWN:
-                keyFrame = Assets.darkBrownBox;
-                break;
-            case COLOR_GRAY:
-                keyFrame = Assets.grayBox;
-                break;
-            case COLOR_DARK_GRAY:
-                keyFrame = Assets.darkGrayBox;
-                break;
-            case COLOR_RED:
-                keyFrame = Assets.redBox;
-                break;
-            case COLOR_DARK_RED:
-                keyFrame = Assets.darkRedBox;
-                break;
-            case COLOR_YELLOW:
-                keyFrame = Assets.yellowBox;
-                break;
-            case COLOR_DARK_YELLOW:
-                keyFrame = Assets.darkYellowBox;
-                break;
-            case COLOR_PURPLE:
-                keyFrame = Assets.purpleBox;
-                break;
-            case COLOR_DARK_PURPLE:
-                keyFrame = Assets.darkPurpleBox;
-                break;
+    private fun setTextureColor(numColor: Int) {
+        when (numColor) {
+            COLOR_BEIGE -> keyFrame = Assets.beigeBox
+            COLOR_DARK_BEIGE -> keyFrame = Assets.darkBeigeBox
+            COLOR_BLACK -> keyFrame = Assets.blackBox
+            COLOR_DARK_BLACK -> keyFrame = Assets.darkBlackBox
+            COLOR_BLUE -> keyFrame = Assets.blueBox
+            COLOR_DARK_BLUE -> keyFrame = Assets.darkBlueBox
+            COLOR_BROWN -> keyFrame = Assets.brownBox
+            COLOR_DARK_BROWN -> keyFrame = Assets.darkBrownBox
+            COLOR_GRAY -> keyFrame = Assets.grayBox
+            COLOR_DARK_GRAY -> keyFrame = Assets.darkGrayBox
+            COLOR_RED -> keyFrame = Assets.redBox
+            COLOR_DARK_RED -> keyFrame = Assets.darkRedBox
+            COLOR_YELLOW -> keyFrame = Assets.yellowBox
+            COLOR_DARK_YELLOW -> keyFrame = Assets.darkYellowBox
+            COLOR_PURPLE -> keyFrame = Assets.purpleBox
+            COLOR_DARK_PURPLE -> keyFrame = Assets.darkPurpleBox
         }
     }
 
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        batch.draw(keyFrame, getX(), getY(), SIZE, SIZE);
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        batch?.draw(keyFrame, x, y, SIZE, SIZE)
     }
 
-    public void setIsInEndPoint(TargetPlatform endPoint) {
-        numColor = Math.abs(numColor);
-        isInRightEndPoint = false;
+    fun setIsInEndPoint(endPoint: TargetPlatform?) {
+        numColor = abs(numColor)
+        isInRightEndPoint = false
         if (endPoint != null && endPoint.numColor == numColor) {
-            numColor = -numColor;
-            isInRightEndPoint = true;
+            numColor = -numColor
+            isInRightEndPoint = true
         }
-        setTextureColor(numColor);
+        setTextureColor(numColor)
+    }
+
+    companion object {
+        const val COLOR_BEIGE = 1
+        const val COLOR_DARK_BEIGE = -1
+        const val COLOR_BLACK = 2
+        const val COLOR_DARK_BLACK = -2
+        const val COLOR_BLUE = 3
+        const val COLOR_DARK_BLUE = -3
+        const val COLOR_BROWN = 4
+        const val COLOR_DARK_BROWN = -4
+        const val COLOR_GRAY = 5
+        const val COLOR_DARK_GRAY = -5
+        const val COLOR_RED = 6
+        const val COLOR_DARK_RED = -6
+        const val COLOR_YELLOW = 7
+        const val COLOR_DARK_YELLOW = -7
+        const val COLOR_PURPLE = 8
+        const val COLOR_DARK_PURPLE = -8
     }
 }
