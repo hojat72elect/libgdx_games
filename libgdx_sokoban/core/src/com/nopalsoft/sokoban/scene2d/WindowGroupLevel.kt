5128fe9eae9b3c91a06adb89c86f57e8e4ac79d6
hojat <hojat72elect@gmail.com>
2025-06-02 23:18:08 -0400

    LevelSelector.java -> kt TouchPadControlsTable.java -> kt WindowGroup.java -> kt WindowGroupLevel.java -> kt WindowGroupPause.java -> kt
    Regarding issue #1

---
+++ libgdx_sokoban/core/src/com/nopalsoft/sokoban/scene2d/WindowGroupLevel.kt
@@ -0,0 +1,65 @@
+package com.nopalsoft.sokoban.scene2d
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.nopalsoft.sokoban.Assets
+import com.nopalsoft.sokoban.game.GameScreen
+import com.nopalsoft.sokoban.screens.Screens
+
+class WindowGroupLevel(currentScreen: Screens) : WindowGroup(currentScreen, 350f, 300f, 100f) {
+    var buttonPlay: Button
+    var labelBestMoves: Label
+    var labelBestTime: Label
+
+    init {
+        setCloseButton()
+        setTitle("Puntuaciones", .75f)
+
+        val tableMenu = Table()
+        tableMenu.setFillParent(true)
+
+        buttonPlay = Button(Assets.buttonPlayDrawable, Assets.buttonPlayPressedDrawable)
+
+        val imageClock = Image(Assets.clockDrawable)
+        val imageMoves = Image(Assets.playerStand)
+
+        labelBestMoves = Label("0", LabelStyle(Assets.fontRed, Color.WHITE))
+        labelBestTime = Label("0", LabelStyle(Assets.fontRed, Color.WHITE))
+
+        tableMenu.defaults().expandX()
+
+        tableMenu.padLeft(30f).padRight(30f).padBottom(20f).padTop(50f)
+        tableMenu.add(imageMoves).size(45f)
+        tableMenu.add(labelBestMoves)
+
+        tableMenu.row().padTop(10f)
+        tableMenu.add(imageClock).size(45f)
+        tableMenu.add(labelBestTime)
+
+        tableMenu.row().padTop(10f)
+        tableMenu.add(buttonPlay).colspan(2).size(60f)
+
+        addActor(tableMenu)
+    }
+
+    fun show(stage: Stage, level: Int, bestMoves: Int, bestTime: Int) {
+        labelBestMoves.setText(bestMoves.toString() + "")
+        labelBestTime.setText(bestTime.toString() + "")
+
+        buttonPlay.clear()
+        buttonPlay.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent, x: Float, y: Float) {
+                screen.changeScreenWithFadeOut(GameScreen::class.java, level, screen.game)
+            }
+        })
+
+        super.show(stage)
+    }
+}
