350e7af7794202c591c9d2be92c903a170e65d0e
hojat <hojat72elect@gmail.com>
2025-06-05 22:46:57 -0400

    Bulk migration of files inside "Ninja Runner" game to kotlin.
    Regarding issue #12

--- libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/NinjaRunnerGame.kt
+++ libgdx_ninja_runner/core/src/com/nopalsoft/ninjarunner/NinjaRunnerGame.kt
@@ -1,36 +1,37 @@
-package com.nopalsoft.ninjarunner;
+package com.nopalsoft.ninjarunner
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.I18NBundle;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.nopalsoft.ninjarunner.game.GameScreen;
-import com.nopalsoft.ninjarunner.leaderboard.Person;
-import com.nopalsoft.ninjarunner.screens.Screens;
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.I18NBundle
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.nopalsoft.ninjarunner.game.GameScreen
+import com.nopalsoft.ninjarunner.leaderboard.Person
+import com.nopalsoft.ninjarunner.screens.Screens
+import com.badlogic.gdx.utils.Array as GdxArray
 
-public class NinjaRunnerGame extends Game {
+class NinjaRunnerGame : Game() {
+    @JvmField
+    var arrayPerson = GdxArray<Person>()
 
-    public Array<Person> arrayPerson = new Array<>();
+    @JvmField
+    var stage: Stage? = null
 
-    public Stage stage;
-    public SpriteBatch batch;
-    public I18NBundle languages;
+    @JvmField
+    var batch: SpriteBatch? = null
 
-    public NinjaRunnerGame() {
-    }
+    @JvmField
+    var languages: I18NBundle? = null
 
-    @Override
-    public void create() {
-        languages = I18NBundle.createBundle(Gdx.files.internal("strings/strings"));
-        batch = new SpriteBatch();
-        stage = new Stage(new StretchViewport(Screens.SCREEN_WIDTH, Screens.SCREEN_HEIGHT));
+    override fun create() {
+        languages = I18NBundle.createBundle(Gdx.files.internal("strings/strings"))
+        batch = SpriteBatch()
+        stage = Stage(StretchViewport(Screens.SCREEN_WIDTH.toFloat(), Screens.SCREEN_HEIGHT.toFloat()))
 
-        Settings.load();
-        Assets.load();
+        Settings.load()
+        Assets.load()
 
-        setScreen(new GameScreen(this, true));
+        setScreen(GameScreen(this, true))
     }
 }
--- libgdx_ninja_runner/desktop/src/com/nopalsoft/ninjarunner/desktop/DesktopLauncher.kt
+++ libgdx_ninja_runner/desktop/src/com/nopalsoft/ninjarunner/desktop/DesktopLauncher.kt
@@ -1,19 +1,14 @@
-package com.nopalsoft.ninjarunner.desktop;
+package com.nopalsoft.ninjarunner.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.nopalsoft.ninjarunner.NinjaRunnerGame;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.ninjarunner.NinjaRunnerGame
 
-public class DesktopLauncher {
-
-    public static NinjaRunnerGame game;
-
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(800, 480);
-        config.setTitle("Ninja Runner");
-        game = new NinjaRunnerGame();
-        new Lwjgl3Application(game, config);
-    }
-
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setWindowedMode(800, 480)
+    config.setTitle("Ninja Runner")
+    val game = NinjaRunnerGame()
+    Lwjgl3Application(game, config)
 }
+
