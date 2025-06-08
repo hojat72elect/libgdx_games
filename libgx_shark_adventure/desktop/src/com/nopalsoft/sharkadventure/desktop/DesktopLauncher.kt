ae7011076262959366a3450256c88ac6615f5e58
hojat <hojat72elect@gmail.com>
2025-06-08 16:51:03 -0400

    Migrated desktop module of "Shark Adventure" game to kotlin.
    Regarding issue #8

--- libgx_shark_adventure/desktop/src/com/nopalsoft/sharkadventure/desktop/DesktopLauncher.kt
+++ libgx_shark_adventure/desktop/src/com/nopalsoft/sharkadventure/desktop/DesktopLauncher.kt
@@ -1,14 +1,13 @@
-package com.nopalsoft.sharkadventure.desktop;
+package com.nopalsoft.sharkadventure.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.sharkadventure.SharkAdventureGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.sharkadventure.SharkAdventureGame
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 800;
-        config.height = 480;
-        new LwjglApplication(new SharkAdventureGame(), config);
-    }
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    config.width = 800
+    config.height = 480
+    LwjglApplication(SharkAdventureGame(), config)
 }
+
