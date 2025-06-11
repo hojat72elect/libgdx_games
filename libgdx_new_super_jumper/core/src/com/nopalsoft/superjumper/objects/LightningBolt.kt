53fa1b49c94367c3f33cccb54cc0920bc4aacbc2
hojat <hojat72elect@gmail.com>
2025-06-10 20:57:08 -0400

    LightningBolt.java -> kt PlatformPiece.java -> kt PowerUpItem.java -> kt
    Regarding issue #13

---
+++ libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/objects/LightningBolt.kt
@@ -0,0 +1,49 @@
+package com.nopalsoft.superjumper.objects
+
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.utils.Pool.Poolable
+
+class LightningBolt : Poolable {
+    @JvmField
+    var state = 0
+
+    @JvmField
+    val position = Vector2()
+
+    @JvmField
+    var stateTime = 0f
+
+    fun init(x: Float, y: Float) {
+        position[x] = y
+        stateTime = 0f
+        state = STATE_NORMAL
+    }
+
+    fun update(body: Body, delta: Float) {
+        position.x = body.position.x
+        position.y = body.position.y
+
+        stateTime += delta
+    }
+
+    fun destroy() {
+        if (state == STATE_NORMAL) {
+            state = STATE_DESTROY
+            stateTime = 0f
+        }
+    }
+
+    override fun reset() {
+    }
+
+    companion object {
+        const val STATE_NORMAL = 0
+        const val STATE_DESTROY = 1
+        const val DRAW_WIDTH = .25f
+        const val DRAW_HEIGHT = .98f
+        const val WIDTH = .21f
+        const val HEIGHT = .93f
+        const val SPEED_Y = -3f
+    }
+}
---
+++ libgdx_new_super_jumper/core/src/com/nopalsoft/superjumper/objects/PlatformPiece.kt
@@ -0,0 +1,65 @@
+package com.nopalsoft.superjumper.objects
+
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.utils.Pool.Poolable
+
+class PlatformPiece : Poolable {
+    @JvmField
+    var state = 0
+
+    @JvmField
+    var color = 0
+
+    @JvmField
+    var type = 0
+
+    @JvmField
+    val position = Vector2()
+
+    var stateTime = 0f
+
+    @JvmField
+    var angleDegree = 0f
+
+    fun initialize(x: Float, y: Float, type: Int, color: Int) {
+        position[x] = y
+        this.type = type
+        this.color = color
+        angleDegree = 0f
+        stateTime = 0f
+        state = STATE_NORMAL
+    }
+
+    fun update(delta: Float, body: Body) {
+        position.x = body.position.x
+        position.y = body.position.y
+        angleDegree = MathUtils.radiansToDegrees * body.angle
+
+        if (angleDegree > 90) {
+            body.setTransform(position, MathUtils.degreesToRadians * 90)
+            angleDegree = 90f
+        }
+
+        stateTime += delta
+    }
+
+    override fun reset() {}
+
+    fun setDestroy() {
+        state = STATE_DESTROY
+    }
+
+    companion object {
+        const val STATE_NORMAL = 0
+        const val STATE_DESTROY = 1
+        const val DRAW_WIDTH_NORMAL = Platform.DRAW_WIDTH_NORMAL / 2f
+        const val DRAW_HEIGHT_NORMAL = Platform.DRAW_HEIGHT_NORMAL
+        const val WIDTH_NORMAL = Platform.WIDTH_NORMAL / 2f
+        const val HEIGHT_NORMAL = Platform.HEIGHT_NORMAL
+
+        const val TYPE_LEFT = 0
+        const val TYPE_RIGHT = 1
+    }
+}
