f4d510f848c59ec35274dde365a0e02ed7ab457f
hojat <hojat72elect@gmail.com>
2025-06-15 23:20:26 -0400

    GameObject.java -> kt
    Regarding issue #20

---
+++ libgdx_galaxy_invaders/core/src/com/nopalsoft/invaders/game_objects/GameObject.kt
@@ -0,0 +1,29 @@
+package com.nopalsoft.invaders.game_objects
+
+import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
+
+open class GameObject {
+    @JvmField
+    val position: Vector2
+
+    @JvmField
+    val boundsRectangle: Rectangle?
+
+    @JvmField
+    var boundsCircle: Circle?
+
+
+    constructor(x: Float, y: Float, width: Float, height: Float) {
+        this.position = Vector2(x, y)
+        this.boundsRectangle = Rectangle(x - width / 2, y - height / 2, width, height)
+        boundsCircle = null
+    }
+
+    constructor(x: Float, y: Float, radio: Float) {
+        this.position = Vector2(x, y)
+        this.boundsRectangle = null
+        boundsCircle = Circle(x, y, radio)
+    }
+}
