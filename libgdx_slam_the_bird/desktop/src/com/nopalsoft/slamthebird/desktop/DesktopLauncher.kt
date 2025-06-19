1225465dca90f76429b530dfd4996bc29fdbcb58
hojat72elect <hojat72elect@gmail.com>
2025-06-18 22:07:42 -0400

    migrated the desktop module to kotlin.
    Regarding issue #22

---
+++ libgdx_slam_the_bird/desktop/src/com/nopalsoft/slamthebird/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.nopalsoft.slamthebird.desktop
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.nopalsoft.slamthebird.SlamTheBirdGame
+
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setWindowedMode(480, 800)
+    config.setTitle("Slam the bird")
+    Lwjgl3Application(SlamTheBirdGame(), config)
+}
+
