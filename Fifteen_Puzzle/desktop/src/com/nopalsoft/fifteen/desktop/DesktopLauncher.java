392913c06b128c5c52f4ceac025e543f7ba64e9f
hojat72elect <hojat72elect@gmail.com>
2025-06-23 04:05:43 -0400

    Fixed build issues in the "Fifteen Puzzle" game.

--- Fifteen_Puzzle/desktop/src/com/nopalsoft/fifteen/desktop/DesktopLauncher.java
+++ Fifteen_Puzzle/desktop/src/com/nopalsoft/fifteen/desktop/DesktopLauncher.java
@@ -7,127 +7,127 @@ import com.nopalsoft.fifteen.handlers.GoogleGameServicesHandler;
 import com.nopalsoft.fifteen.handlers.RequestHandler;
 
 public class DesktopLauncher {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "FifteenPuzzle";
-		cfg.width = 480;
-		cfg.height = 800;
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "FifteenPuzzle";
+        cfg.width = 480;
+        cfg.height = 800;
 
-		new LwjglApplication(
-				new MainFifteen( handler, gameHandler), cfg);
-	}
+        new LwjglApplication(
+                new MainFifteen(handler, gameHandler), cfg);
+    }
 
-	static RequestHandler handler = new RequestHandler() {
+    static RequestHandler handler = new RequestHandler() {
 
-		@Override
-		public void showRater() {
-			// TODO Auto-generated method stub
+        @Override
+        public void showRater() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void showMoreGames() {
-			// TODO Auto-generated method stub
+        @Override
+        public void showMoreGames() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void showInterstitial() {
-			// TODO Auto-generated method stub
+        @Override
+        public void showInterstitial() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void showFacebook() {
-			// TODO Auto-generated method stub
+        @Override
+        public void showFacebook() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void showAdBanner() {
-			// TODO Auto-generated method stub
+        @Override
+        public void showAdBanner() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void shareOnTwitter(String mensaje) {
-			// TODO Auto-generated method stub
+        @Override
+        public void shareOnTwitter(String mensaje) {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void shareOnFacebook(String mensaje) {
-			// TODO Auto-generated method stub
+        @Override
+        public void shareOnFacebook(String mensaje) {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void restorePurchases() {
-			// TODO Auto-generated method stub
+        @Override
+        public void restorePurchases() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void removeAds() {
-			// TODO Auto-generated method stub
+        @Override
+        public void removeAds() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void hideAdBanner() {
-			// TODO Auto-generated method stub
+        @Override
+        public void hideAdBanner() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void buyNoAds() {
-			// TODO Auto-generated method stub
+        @Override
+        public void buyNoAds() {
+            // TODO Auto-generated method stub
 
-		}
-	};
+        }
+    };
 
-	static GoogleGameServicesHandler gameHandler = new GoogleGameServicesHandler() {
+    static GoogleGameServicesHandler gameHandler = new GoogleGameServicesHandler() {
 
-		@Override
-		public void unlockAchievement(String achievementId) {
-			// TODO Auto-generated method stub
+        @Override
+        public void unlockAchievement(String achievementId) {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void submitScore(long score, String leaderboard) {
-			// TODO Auto-generated method stub
+        @Override
+        public void submitScore(long score, String leaderboard) {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void signOut() {
-			// TODO Auto-generated method stub
+        @Override
+        public void signOut() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void signIn() {
-			// TODO Auto-generated method stub
+        @Override
+        public void signIn() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public boolean isSignedIn() {
-			// TODO Auto-generated method stub
-			return false;
-		}
+        @Override
+        public boolean isSignedIn() {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-		@Override
-		public void getLeaderboard() {
-			// TODO Auto-generated method stub
+        @Override
+        public void getLeaderboard() {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
-		@Override
-		public void getAchievements() {
-			// TODO Auto-generated method stub
+        @Override
+        public void getAchievements() {
+            // TODO Auto-generated method stub
 
-		}
-	};
+        }
+    };
 }
