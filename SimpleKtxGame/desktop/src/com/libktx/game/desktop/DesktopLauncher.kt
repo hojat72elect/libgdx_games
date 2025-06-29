85723ebdc932e5b3983f14bbed0598a13b9aec8b
hojat72elect <hojat72elect@gmail.com>
2025-06-28 21:41:11 -0400

    Cleaned a bit

--- SimpleKtxGame/desktop/src/com/libktx/game/desktop/DesktopLauncher.kt
+++ SimpleKtxGame/desktop/src/com/libktx/game/desktop/DesktopLauncher.kt
@@ -5,14 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.libktx.game.Game
 
-object DesktopLauncher {
-    @JvmStatic
-    fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration().apply {
-            title = "Drop"
-            width = 800
-            height = 480
-        }
-        LwjglApplication(Game(), config).logLevel = Application.LOG_DEBUG
+
+fun main() {
+    val config = LwjglApplicationConfiguration().apply {
+        title = "Drop"
+        width = 800
+        height = 480
     }
+    LwjglApplication(Game(), config).logLevel = Application.LOG_DEBUG
 }
+
