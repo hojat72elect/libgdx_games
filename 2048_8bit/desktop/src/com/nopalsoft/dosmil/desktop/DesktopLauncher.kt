d3aeffc1b8c4e82cd6b7bd6ac358dae62c63141c
hojat72elect <hojat72elect@gmail.com>
2025-07-13 13:34:56 -0400

    2048_8bit : Migrated the desktop module to kotlin.

--- 2048_8bit/desktop/src/com/nopalsoft/dosmil/desktop/DesktopLauncher.kt
+++ 2048_8bit/desktop/src/com/nopalsoft/dosmil/desktop/DesktopLauncher.kt
@@ -1,17 +1,15 @@
-package com.nopalsoft.dosmil.desktop;
+package com.nopalsoft.dosmil.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.dosmil.MainGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.dosmil.MainGame
 
-public class DesktopLauncher {
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    config.title = "2048 8Bit"
+    config.width = 480
+    config.height = 800
 
-    public static void main(String[] args) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "2048 8Bit";
-        config.width = 480;
-        config.height = 800;
-
-        new LwjglApplication(new MainGame(), config);
-    }
+    LwjglApplication(MainGame(), config)
 }
+
