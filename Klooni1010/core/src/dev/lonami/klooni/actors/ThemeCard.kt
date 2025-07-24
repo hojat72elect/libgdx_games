64ba2ccab06e11149b7b2215c559c85f1e2402d6
hojat72elect <hojat72elect@gmail.com>
2025-07-24 16:52:26 -0400

    ThemeCard.java -> kt

---
+++ Klooni1010/core/src/dev/lonami/klooni/actors/ThemeCard.kt
@@ -0,0 +1,83 @@
+package dev.lonami.klooni.actors
+
+import com.badlogic.gdx.graphics.g2d.Batch
+import dev.lonami.klooni.Klooni
+import dev.lonami.klooni.Klooni.Companion.buyTheme
+import dev.lonami.klooni.Klooni.Companion.isThemeBought
+import dev.lonami.klooni.Klooni.Companion.updateTheme
+import dev.lonami.klooni.Theme
+import dev.lonami.klooni.Theme.Companion.getBlankTexture
+import dev.lonami.klooni.game.Cell
+import dev.lonami.klooni.game.GameLayout
+
+/**
+ * Card-like actor used to display information about a given theme
+ */
+class ThemeCard(
+    game: Klooni,
+    layout: GameLayout,
+    private val theme: Theme
+) : ShopCard(game, layout, theme.displayName, theme.background) {
+    private val background = getBlankTexture()
+
+    init {
+        usedItemUpdated()
+    }
+
+    override fun draw(batch: Batch, parentAlpha: Float) {
+        val x = getX()
+        val y = getY()
+
+        batch.color = theme.background
+        batch.draw(background, x, y, getWidth(), getHeight())
+
+        // Avoid drawing on the borders by adding +1 cell padding
+        for (i in colorsUsed.indices) {
+            for (j in colorsUsed[i]!!.indices) {
+                Cell.draw(
+                    theme.cellTexture, theme.getCellColor(colorsUsed[i]!![j]), batch,
+                    x + cellSize * (j + 1), y + cellSize * (i + 1), cellSize
+                )
+            }
+        }
+
+        super.draw(batch, parentAlpha)
+    }
+
+    override fun usedItemUpdated() {
+        if (Klooni.theme!!.name == theme.name) priceLabel.setText("currently used")
+        else if (isThemeBought(theme)) priceLabel.setText("bought")
+        else priceLabel.setText("buy for " + theme.price)
+    }
+
+    override fun use() {
+        updateTheme(theme)
+        usedItemUpdated()
+    }
+
+    override fun isBought(): Boolean {
+        return isThemeBought(theme)
+    }
+
+    override fun isUsed(): Boolean {
+        return Klooni.theme!!.name == theme.name
+    }
+
+    override fun getPrice(): Float {
+        return theme.price.toFloat()
+    }
+
+    override fun performBuy() {
+        if (buyTheme(theme)) {
+            use()
+        }
+    }
+
+    companion object {
+        private val colorsUsed = arrayOf<IntArray?>(
+            intArrayOf(0, 7, 7),
+            intArrayOf(8, 7, 3),
+            intArrayOf(8, 8, 3)
+        )
+    }
+}
