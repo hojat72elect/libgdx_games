056f9cb4b1479dadf76a04b2b7c7985912a0bafe
hojat72elect <hojat72elect@gmail.com>
2025-07-24 02:03:14 -0400

    IEffect.java -> kt IEffectFactory.java -> kt

---
+++ Klooni1010/core/src/dev/lonami/klooni/interfaces/IEffectFactory.kt
@@ -0,0 +1,23 @@
+package dev.lonami.klooni.interfaces
+
+import com.badlogic.gdx.math.Vector2
+import dev.lonami.klooni.game.Cell
+
+/**
+ * IEffectFactory interface has to be implemented for each effect.
+ *
+ *
+ * It tells the name and the price of the effect and will create it, when needed.
+ *
+ * @see IEffect
+ */
+interface IEffectFactory {
+
+    val name: String
+
+    val display: String
+
+    val price: Int
+
+    fun create(deadCell: Cell, culprit: Vector2): IEffect
+}
