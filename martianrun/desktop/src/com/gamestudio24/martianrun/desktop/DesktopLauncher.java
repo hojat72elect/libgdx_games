5bfbe96dfb6e382aeb526fc6cff8aba7a5d807af
hojat72elect <hojat72elect@gmail.com>
2025-07-25 19:56:26 -0400

    Removed the android module from the "Martian Run" game.

--- martianrun/desktop/src/com/gamestudio24/martianrun/desktop/DesktopLauncher.java
+++ martianrun/desktop/src/com/gamestudio24/martianrun/desktop/DesktopLauncher.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2014. William Mora
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.gamestudio24.martianrun.desktop;
 
 import com.badlogic.gdx.Gdx;
@@ -24,11 +8,11 @@ import com.gamestudio24.martianrun.utils.Constants;
 import com.gamestudio24.martianrun.utils.GameEventListener;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Constants.APP_WIDTH;
         config.height = Constants.APP_HEIGHT;
-		new LwjglApplication(new MartianRun(new GameEventListener() {
+        new LwjglApplication(new MartianRun(new GameEventListener() {
             @Override
             public void displayAd() {
                 Gdx.app.log(GameEventListener.class.getSimpleName(), "displayAd");
@@ -123,7 +107,6 @@ public class DesktopLauncher {
             public String get500JumpStreetAchievementId() {
                 return null;
             }
-
         }), config);
-	}
+    }
 }
