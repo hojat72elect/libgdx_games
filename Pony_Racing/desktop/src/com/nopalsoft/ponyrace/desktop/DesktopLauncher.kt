672dafa85731dc28a96a42581089f508888d4f61
hojat72elect <hojat72elect@gmail.com>
2025-07-27 02:45:24 -0400

    "Pony Racing" cleaned a bit

--- Pony_Racing/desktop/src/com/nopalsoft/ponyrace/desktop/DesktopLauncher.kt
+++ Pony_Racing/desktop/src/com/nopalsoft/ponyrace/desktop/DesktopLauncher.kt
@@ -5,16 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.nopalsoft.ponyrace.MainPonyRace
 import com.nopalsoft.ponyrace.handlers.FloatFormatter
 
-object DesktopLauncher {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val cfg = LwjglApplicationConfiguration()
-        cfg.title = "Pony Racing"
-        cfg.width = 800
-        cfg.height = 480
-        LwjglApplication(MainPonyRace(formatter), cfg)
-    }
+val formatter = FloatFormatter { format: String, args: Float -> String.format(format, args) }
 
-    var formatter: FloatFormatter = FloatFormatter { format: String?, args: Float -> String.format(format!!, args) }
+fun main(args: Array<String>) {
+    val cfg = LwjglApplicationConfiguration()
+    cfg.title = "Pony Racing"
+    cfg.width = 800
+    cfg.height = 480
+    LwjglApplication(MainPonyRace(formatter), cfg)
 }
