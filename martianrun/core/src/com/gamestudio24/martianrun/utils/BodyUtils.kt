596822f48a09ada8a7d9ab9bcf6653365e8ecf0b
hojat72elect <hojat72elect@gmail.com>
2025-07-29 00:40:14 -0400

    "Martian Run" :
    BodyUtils.java -> kt

---
+++ martianrun/core/src/com/gamestudio24/martianrun/utils/BodyUtils.kt
@@ -0,0 +1,34 @@
+package com.gamestudio24.martianrun.utils
+
+import com.badlogic.gdx.physics.box2d.Body
+import com.gamestudio24.martianrun.box2d.UserData
+import com.gamestudio24.martianrun.enums.UserDataType
+
+object BodyUtils {
+    fun bodyInBounds(body: Body): Boolean {
+        val userData = body.userData as UserData
+
+        return when (userData.getUserDataType()) {
+            UserDataType.RUNNER, UserDataType.ENEMY -> body.getPosition().x + userData.getWidth() / 2 > 0
+            else -> true
+        }
+    }
+
+    fun bodyIsEnemy(body: Body): Boolean {
+        val userData = body.userData as UserData?
+
+        return userData != null && userData.getUserDataType() == UserDataType.ENEMY
+    }
+
+    fun bodyIsRunner(body: Body): Boolean {
+        val userData = body.userData as UserData?
+
+        return userData != null && userData.getUserDataType() == UserDataType.RUNNER
+    }
+
+    fun bodyIsGround(body: Body): Boolean {
+        val userData = body.userData as UserData?
+
+        return userData != null && userData.getUserDataType() == UserDataType.GROUND
+    }
+}
