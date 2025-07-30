047e9887cffe23ccb4e3164b9956625824439ac5
hojat72elect <hojat72elect@gmail.com>
2025-07-30 00:41:27 -0400

    "whatcolor" : Migrated the desktop module to kotlin.

--- whatcolor/desktop/src/com/salvai/whatcolor/desktop/DesktopLauncher.kt
+++ whatcolor/desktop/src/com/salvai/whatcolor/desktop/DesktopLauncher.kt
@@ -1,12 +1,12 @@
-package com.salvai.whatcolor.desktop;
+package com.salvai.whatcolor.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.salvai.whatcolor.WhatColor;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.salvai.whatcolor.WhatColor
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        new Lwjgl3Application(new WhatColor(), config);
-    }
+
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    Lwjgl3Application(WhatColor(), config)
 }
+
