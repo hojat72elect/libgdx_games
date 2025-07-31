e0a639e109c766ea41ea260a123dbd04e8d044eb
hojat72elect <hojat72elect@gmail.com>
2025-07-31 19:46:22 -0400

    "Zombie Dash" : Migrated desktop module to kotlin.

--- Zombie_Dash/desktop/src/com/nopalsoft/zombiedash/desktop/DesktopLauncher.kt
+++ Zombie_Dash/desktop/src/com/nopalsoft/zombiedash/desktop/DesktopLauncher.kt
@@ -1,14 +1,12 @@
-package com.nopalsoft.zombiedash.desktop;
+package com.nopalsoft.zombiedash.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.zombiedash.MainZombieDash;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.zombiedash.MainZombieDash
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 800;
-        config.height = 480;
-        new LwjglApplication(new MainZombieDash(), config);
-    }
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    config.width = 800
+    config.height = 480
+    LwjglApplication(MainZombieDash(), config)
 }
