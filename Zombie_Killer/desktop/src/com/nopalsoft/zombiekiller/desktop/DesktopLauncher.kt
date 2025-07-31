09c8c252f26ca75e4f0435dd90a5418cf712dfa2
hojat72elect <hojat72elect@gmail.com>
2025-07-30 23:01:55 -0400

    "Zombie Killer" : Migrated the desktop module to kotlin.

--- Zombie_Killer/desktop/src/com/nopalsoft/zombiekiller/desktop/DesktopLauncher.kt
+++ Zombie_Killer/desktop/src/com/nopalsoft/zombiekiller/desktop/DesktopLauncher.kt
@@ -1,14 +1,12 @@
-package com.nopalsoft.zombiekiller.desktop;
+package com.nopalsoft.zombiekiller.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.zombiekiller.MainZombie;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.zombiekiller.MainZombie
 
-public class DesktopLauncher {
-    public static void main(String[] args) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 800;
-        config.height = 480;
-        new LwjglApplication(new MainZombie(), config);
-    }
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    config.width = 800
+    config.height = 480
+    LwjglApplication(MainZombie(), config)
 }
