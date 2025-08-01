1b4f8731c6371976b84e7093bea45dcc6ce99322
hojat72elect <hojat72elect@gmail.com>
2025-08-01 17:12:17 -0400

    "sn4ke" : Migrated desktop module to kotlin.

--- sn4ke/desktop/src/com/salvai/snake/desktop/DesktopLauncher.kt
+++ sn4ke/desktop/src/com/salvai/snake/desktop/DesktopLauncher.kt
@@ -1,12 +1,10 @@
-package com.salvai.snake.desktop;
+package com.salvai.snake.desktop
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.salvai.snake.SnakeIt;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.salvai.snake.SnakeIt
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        new Lwjgl3Application(new SnakeIt(), config);
-    }
+fun main() {
+    val config = Lwjgl3ApplicationConfiguration()
+    Lwjgl3Application(SnakeIt(), config)
 }
