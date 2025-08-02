089215b87ca893cea7908e9c448448846ef6d415
hojat72elect <hojat72elect@gmail.com>
2025-08-01 23:46:27 -0400

    "Fifteen Puzzle" : migration to kotlin (part 2)

--- Fifteen_Puzzle/desktop/src/com/nopalsoft/fifteen/desktop/DesktopLauncher.kt
+++ Fifteen_Puzzle/desktop/src/com/nopalsoft/fifteen/desktop/DesktopLauncher.kt
@@ -1,17 +1,16 @@
-package com.nopalsoft.fifteen.desktop;
+package com.nopalsoft.fifteen.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.fifteen.MainFifteen;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.fifteen.MainFifteen
 
-public class DesktopLauncher {
-    public static void main(String[] args) {
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "FifteenPuzzle";
-        cfg.width = 480;
-        cfg.height = 800;
+fun main() {
+    val cfg = LwjglApplicationConfiguration()
+    cfg.title = "FifteenPuzzle"
+    cfg.width = 480
+    cfg.height = 800
 
-        new LwjglApplication(
-                new MainFifteen(), cfg);
-    }
+    LwjglApplication(
+        MainFifteen(), cfg
+    )
 }
