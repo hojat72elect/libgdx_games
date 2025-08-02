efdf05f9e3d1f888935f93a190850251e8990fb2
hojat72elect <hojat72elect@gmail.com>
2025-08-01 20:28:22 -0400

    "sn4ke" : Migrated to kotlin (part 10)

---
+++ sn4ke/core/src/com/salvai/snake/levels/LevelReader.kt
@@ -0,0 +1,32 @@
+package com.salvai.snake.levels
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.utils.Array
+import com.badlogic.gdx.utils.Json
+import com.salvai.snake.utils.Constants
+
+class LevelReader {
+    private val json: Json = Json()
+
+    fun loadAllLevels(): Array<Level?> {
+        val levels = Array<Level?>()
+
+        for (i in 1..Constants.MAX_LEVEL) levels.add(loadLevel(i))
+        return levels
+    }
+
+    private fun loadLevel(level: Int): Level? {
+        if (level % 20 == 0) {
+            return json.fromJson(
+                Level::class.java,
+                Gdx.files.internal("levels/level" + level / Constants.LEVELS_PRO_TAB + "-20.json")
+            )
+            //TODO find out how to read levels correctly
+        } else {
+            return json.fromJson(
+                Level::class.java,
+                Gdx.files.internal("levels/level" + ((level / Constants.LEVELS_PRO_TAB) + 1) + "-" + level % Constants.LEVELS_PRO_TAB + ".json")
+            ) //TODO find out how to read levels correctly}
+        }
+    }
+}
