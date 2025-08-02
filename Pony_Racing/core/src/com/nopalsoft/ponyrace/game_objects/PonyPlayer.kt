ed30a852416c81d7108bec8adb8ac954d5dd65e7
hojat72elect <hojat72elect@gmail.com>
2025-08-02 03:58:11 -0400

    "Pony Racing" : Migrated the game to kotlin (part 2).

---
+++ Pony_Racing/core/src/com/nopalsoft/ponyrace/game_objects/PonyPlayer.kt
@@ -0,0 +1,5 @@
+package com.nopalsoft.ponyrace.game_objects
+
+import com.nopalsoft.ponyrace.game.TileMapHandler
+
+class PonyPlayer(x: Float, y: Float, nombreSkin: String, oWorld: TileMapHandler) : Pony(x, y, nombreSkin, oWorld)
