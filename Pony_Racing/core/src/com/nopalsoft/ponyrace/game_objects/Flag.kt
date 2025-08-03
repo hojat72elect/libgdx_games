3e05a2ce4f33c912ebeaca5e408b323b319d88fb
hojat72elect <hojat72elect@gmail.com>
2025-08-03 17:16:17 -0400

    "Pony Racing" :
    Flag.java -> kt

---
+++ Pony_Racing/core/src/com/nopalsoft/ponyrace/game_objects/Flag.kt
@@ -0,0 +1,35 @@
+package com.nopalsoft.ponyrace.game_objects
+
+import com.nopalsoft.ponyrace.game.TileMapHandler
+import java.util.Random
+
+class Flag(world: TileMapHandler, @JvmField var actionType: ActionType?) {
+
+    @JvmField
+    var state: State?
+    var random: Random
+
+    init {
+        random = world.random
+        state = State.NORMAL
+    }
+
+    fun permitirSalto(): Boolean {
+        if (state == State.NORMAL && random.nextBoolean()) {
+            state = State.ACTIVE
+            return true
+        }
+        return false
+    }
+
+    enum class State {
+        NORMAL,
+        ACTIVE
+    }
+
+    enum class ActionType {
+        JUMP_LEFT,
+        JUMP_RIGHT,
+        JUMP
+    }
+}
