ef3df50a50033c23e9c5fb7793a29133d85a8272
hojat72elect <hojat72elect@gmail.com>
2025-08-09 02:40:32 -0400

    "Spine Runtime" : docs

--- Pony_Racing/core/src/com/esotericsoftware/spine/BoneData.java
+++ Pony_Racing/core/src/com/esotericsoftware/spine/BoneData.java
@@ -2,10 +2,14 @@ package com.esotericsoftware.spine;
 
 import com.badlogic.gdx.graphics.Color;
 
+/**
+ * Properties of a single bone in skeletal animations.
+ * Bones in a skeletal animation system are usually hierarchical, which means bones can have parent bones.
+ * This class only works for 2D skeletal animations (Spine app).
+ */
 public class BoneData {
     final BoneData parent;
     final String name;
-    // Nonessential.
     final Color color = new Color(1, 1, 1, 1);
     float length;
     float x, y;
