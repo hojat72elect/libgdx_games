871fdb53cf62d5d6cb65ee1b269047bf2ac867af
hojat72elect <hojat72elect@gmail.com>
2025-08-10 15:18:04 -0400

    Cleaned a bit.

--- Lander/core/src/com/nopalsoft/lander/screens/MainMenuScreen.kt
+++ Lander/core/src/com/nopalsoft/lander/screens/MainMenuScreen.kt
@@ -14,16 +14,14 @@ import com.nopalsoft.lander.dialogs.VentanaShop
 
 class MainMenuScreen(game: MainLander) : Screens(game) {
     var btPlay = TextButton("Play", Assets.styleTextButtonMenu)
-    var btSettings: TextButton? = null
-    var btMore: TextButton? = null
-
-    var dialogShop: VentanaShop
+    var btSettings = TextButton("Settings", Assets.styleTextButtonMenu)
+    var btMore = TextButton("More", Assets.styleTextButtonMenu)
+    var dialogShop = VentanaShop(game)
 
     init {
 
         val botonWidth = 300f
         val botonX = SCREEN_WIDTH / 2f - botonWidth / 2f
-
 
         btPlay.setSize(botonWidth, 100f)
         btPlay.setPosition(botonX, -10f)
@@ -33,22 +31,20 @@ class MainMenuScreen(game: MainLander) : Screens(game) {
             }
         })
 
-        btSettings = TextButton("Settings", Assets.styleTextButtonMenu)
-        btSettings!!.setSize(botonWidth, 100f)
-        btSettings!!.setPosition(botonX, -50f)
-        btSettings!!.addListener(object : ClickListener() {
+        btSettings.setSize(botonWidth, 100f)
+        btSettings.setPosition(botonX, -50f)
+        btSettings.addListener(object : ClickListener() {
             override fun clicked(event: InputEvent?, x: Float, y: Float) {
                 dialogShop.show(stage)
             }
         })
 
-        btMore = TextButton("More", Assets.styleTextButtonMenu)
-        btMore!!.setSize(botonWidth, 100f)
-        btMore!!.setPosition(botonX, -90f)
+        btMore.setSize(botonWidth, 100f)
+        btMore.setPosition(botonX, -90f)
 
         addActionToButtonEnter(btPlay, botonX, 420f)
-        addActionToButtonEnter(btSettings!!, botonX, 300f)
-        addActionToButtonEnter(btMore!!, botonX, 180f)
+        addActionToButtonEnter(btSettings, botonX, 300f)
+        addActionToButtonEnter(btMore, botonX, 180f)
 
         val action = Actions.action(MoveToAction::class.java)
         action.interpolation = Interpolation.linear
@@ -68,8 +64,6 @@ class MainMenuScreen(game: MainLander) : Screens(game) {
         stage.addActor(btSettings)
         stage.addActor(btMore)
         stage.addActor(titulo)
-
-        dialogShop = VentanaShop(game)
     }
 
     fun addActionToButtonEnter(bt: TextButton, x: Float, y: Float) {
@@ -82,8 +76,8 @@ class MainMenuScreen(game: MainLander) : Screens(game) {
 
     fun changeScreen(screen: Class<*>) {
         addActionToButtonLeave(btPlay, btPlay.getX(), -100f)
-        addActionToButtonLeave(btSettings!!, btSettings!!.getX(), -100f)
-        addActionToButtonLeave(btMore!!, btMore!!.getX(), -100f)
+        addActionToButtonLeave(btSettings, btSettings.getX(), -100f)
+        addActionToButtonLeave(btMore, btMore.getX(), -100f)
 
         stage.addAction(Actions.sequence(Actions.delay(.75f), Actions.run {
             if (screen == LevelScreen::class.java) {
