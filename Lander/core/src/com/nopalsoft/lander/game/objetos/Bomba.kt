544d6ce64d08d961e389c352f8cb2c85da7a0432
hojat72elect <hojat72elect@gmail.com>
2025-08-10 04:43:16 -0400

    Cleaned a bit.

--- Lander/core/src/com/nopalsoft/lander/game/objetos/Bomba.kt
+++ Lander/core/src/com/nopalsoft/lander/game/objetos/Bomba.kt
@@ -3,51 +3,47 @@ package com.nopalsoft.lander.game.objetos
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Body
 
-class Bomba(x: Float, y: Float, direccion: String) {
+class Bomba(x: Float, y: Float, directionString: String) {
 
     @JvmField
-    var position: Vector2 = Vector2(x, y)
+    var position = Vector2(x, y)
+
+    lateinit var velocidad: Vector2
 
     @JvmField
-    var velocidad: Vector2? = null
+    var stateTime = 0F
 
     @JvmField
-    var stateTime: Float
-
-    @JvmField
-    var state: Int
-    var direccion: Int = 0
+    var state = STATE_NORMAL
+    var directionInteger = 0
 
     init {
-        stateTime = 0f
-        state = STATE_NORMAL
-
-        when (direccion) {
+        when (directionString) {
             "arriba" -> {
-                this.direccion = DIRECCION_UP
+                this.directionInteger = DIRECCION_UP
                 velocidad = Vector2(0f, VELOCIDAD)
             }
 
             "abajo" -> {
-                this.direccion = DIRECCION_UP
+                this.directionInteger = DIRECCION_UP
                 velocidad = Vector2(0f, -VELOCIDAD)
             }
 
             "derecha" -> {
-                this.direccion = DIRECCION_RIGHT
+                this.directionInteger = DIRECCION_RIGHT
                 velocidad = Vector2(VELOCIDAD, 0f)
             }
 
             "izquierda" -> {
-                this.direccion = DIRECCION_RIGHT
+                this.directionInteger = DIRECCION_RIGHT
                 velocidad = Vector2(-VELOCIDAD, 0f)
             }
         }
     }
 
     fun update(delta: Float, body: Body) {
-        position.x = body.getPosition().x
-        position.y = body.getPosition().y
+        position.x = body.position.x
+        position.y = body.position.y
 
         if (state == STATE_TOMADA && stateTime > TIME_TOMADA) {
             state = STATE_EXPLOSION
@@ -58,18 +54,18 @@ class Bomba(x: Float, y: Float, direccion: String) {
     }
 
     fun cambioDireccion() {
-        velocidad!!.x *= -1f
-        velocidad!!.y *= -1f
+        velocidad.x *= -1f
+        velocidad.y *= -1f
     }
 
     companion object {
-        private const val TIME_TOMADA: Float = .5f
-        private const val VELOCIDAD: Float = 1f
-        const val STATE_NORMAL: Int = 0
-        const val STATE_TOMADA: Int = 1
-        const val STATE_EXPLOSION: Int = 2
-        const val TIME_EXPLOSION: Float = .05f * 20
-        private const val DIRECCION_UP: Int = 0
-        private const val DIRECCION_RIGHT: Int = 1
+        private const val TIME_TOMADA = .5F
+        private const val VELOCIDAD = 1F
+        const val STATE_NORMAL = 0
+        const val STATE_TOMADA = 1
+        const val STATE_EXPLOSION = 2
+        const val TIME_EXPLOSION = .05F * 20
+        private const val DIRECCION_UP = 0
+        private const val DIRECCION_RIGHT = 1
     }
 }
