6931289560e1aa0b57913228232b4b7167500be3
hojat72elect <hojat72elect@gmail.com>
2025-08-10 14:04:29 -0400

    Cleaned a bit

--- Lander/core/src/com/nopalsoft/lander/Assets.kt
+++ Lander/core/src/com/nopalsoft/lander/Assets.kt
@@ -20,78 +20,35 @@ import com.nopalsoft.lander.Settings.load
 
 object Assets {
     @JvmField
-    var isDebug: Boolean = false
+    var isDebug = false
 
-    @JvmField
-    var titulo: TextureRegionDrawable? = null
-
-    @JvmField
-    var nave: AtlasRegion? = null
-
-    @JvmField
-    var naveFly: Animation<TextureRegion?>? = null
-
-    @JvmField
-    var explosion: Animation<TextureRegion?>? = null
-
-    @JvmField
-    var laser: Animation<TextureRegion?>? = null
-
-    @JvmField
-    var laserVertical: Animation<TextureRegion?>? = null
-
-    @JvmField
-    var fondo: AtlasRegion? = null
-
-    @JvmField
-    var candado: AtlasRegion? = null
-
-    @JvmField
-    var gas: AtlasRegion? = null
-
-    @JvmField
-    var star: AtlasRegion? = null
-
-    @JvmField
-    var starOff: AtlasRegion? = null
-
-    @JvmField
-    var bomba: AtlasRegion? = null
-
-    @JvmField
-    var upgradeOn: AtlasRegion? = null
-
-    @JvmField
-    var upgradeOff: AtlasRegion? = null
-
-    @JvmField
-    var marcoStats: TextureRegionDrawable? = null
-    var barraMarcadorRojo: AtlasRegion? = null
-    var barraMarcadorVerde: AtlasRegion? = null
-
-    var font: BitmapFont? = null
-    var mundos: LinkedHashMap<Int?, String?>? = null
-
+    lateinit var titulo: TextureRegionDrawable
+    lateinit var nave: AtlasRegion
+    lateinit var naveFly: Animation<TextureRegion>
+    lateinit var explosion: Animation<TextureRegion>
+    lateinit var laser: Animation<TextureRegion>
+    lateinit var laserVertical: Animation<TextureRegion>
+    lateinit var fondo: AtlasRegion
+    lateinit var candado: AtlasRegion
+    lateinit var gas: AtlasRegion
+    lateinit var star: AtlasRegion
+    lateinit var starOff: AtlasRegion
+    lateinit var bomba: AtlasRegion
+    lateinit var upgradeOn: AtlasRegion
+    lateinit var upgradeOff: AtlasRegion
+    lateinit var marcoStats: TextureRegionDrawable
+    lateinit var barraMarcadorRojo: AtlasRegion
+    lateinit var barraMarcadorVerde: AtlasRegion
+    lateinit var font: BitmapFont
+    lateinit var mundos: LinkedHashMap<Int, String>
     @JvmField
     var map: TiledMap? = null
-
-    @JvmField
-    var styleTextButtonMenu: TextButtonStyle? = null
-
-    @JvmField
-    var styleTextButtonLevels: TextButtonStyle? = null
-
-    @JvmField
-    var styleLabelMediana: LabelStyle? = null
-
-    @JvmField
-    var styleImageButtonPause: ImageButtonStyle? = null
-
-    @JvmField
-    var styleImageButtonUpgradePlus: ImageButtonStyle? = null
-
-    @JvmField
-    var styleDialogGameOver: WindowStyle? = null
+    lateinit var styleTextButtonMenu: TextButtonStyle
+    lateinit var styleTextButtonLevels: TextButtonStyle
+    lateinit var styleLabelMediana: LabelStyle
+    lateinit var styleImageButtonPause: ImageButtonStyle
+    lateinit var styleImageButtonUpgradePlus: ImageButtonStyle
+    lateinit var styleDialogGameOver: WindowStyle
 
     private fun loadSceneStyles(atlas: TextureAtlas) {
         styleLabelMediana = LabelStyle(font, Color.BLACK)
@@ -100,7 +57,7 @@ object Assets {
         val botonMenu = TextureRegionDrawable(atlas.findRegion("btMenu"))
         val botonMenuDown = TextureRegionDrawable(atlas.findRegion("btMenuDown"))
         styleTextButtonMenu = TextButtonStyle(botonMenu, botonMenuDown, null, font)
-        styleTextButtonMenu!!.fontColor = Color.GREEN
+        styleTextButtonMenu.fontColor = Color.GREEN
 
         // Boton + de la ventana upgrades
         val btUp = TextureRegionDrawable(atlas.findRegion("btUpgrade"))
@@ -111,7 +68,7 @@ object Assets {
         val recuadroGame = NinePatchDrawable(atlas.createPatch("recuadroGameOver"))
         val dialogDim = atlas.findRegion("dim")
         styleDialogGameOver = WindowStyle(font, Color.GREEN, recuadroGame)
-        styleDialogGameOver!!.stageBackground = NinePatchDrawable(NinePatch(dialogDim))
+        styleDialogGameOver.stageBackground = NinePatchDrawable(NinePatch(dialogDim))
 
         // Botton Pausa
         val btPause = TextureRegionDrawable(atlas.findRegion("btPause"))
@@ -122,7 +79,7 @@ object Assets {
         val btLevels = NinePatchDrawable(atlas.createPatch("btnLevels"))
         val btLevelsDown = NinePatchDrawable(atlas.createPatch("btLevelsDown"))
         styleTextButtonLevels = TextButtonStyle(btLevels, btLevelsDown, null, font)
-        styleTextButtonLevels!!.fontColor = Color.GREEN
+        styleTextButtonLevels.fontColor = Color.GREEN
     }
 
     fun cargar() {
@@ -140,7 +97,7 @@ object Assets {
         val an3 = atlas.findRegion("nave3")
         val an4 = atlas.findRegion("nave4")
         val an5 = atlas.findRegion("nave5")
-        naveFly = Animation<TextureRegion?>(.15f, an1, an2, an3, an4, an5)
+        naveFly = Animation<TextureRegion>(.15f, an1, an2, an3, an4, an5)
 
         barraMarcadorRojo = atlas.findRegion("barraMarcadorRojo")
         barraMarcadorVerde = atlas.findRegion("barraMarcadorVerde")
@@ -167,7 +124,7 @@ object Assets {
         val newExpl17 = atlas.findRegion("newExplosion17")
         val newExpl18 = atlas.findRegion("newExplosion18")
         val newExpl19 = atlas.findRegion("newExplosion19")
-        explosion = Animation<TextureRegion?>(
+        explosion = Animation<TextureRegion>(
             0.05f,
             newExpl1,
             newExpl2,
@@ -194,13 +151,13 @@ object Assets {
         var laser1 = atlas.findRegion("rayo1")
         var laser2 = atlas.findRegion("rayo2")
         var laser3 = atlas.findRegion("rayo3")
-        laser = Animation<TextureRegion?>(0.1f, laser0, laser1, laser2, laser3)
+        laser = Animation<TextureRegion>(0.1f, laser0, laser1, laser2, laser3)
 
         laser0 = atlas.findRegion("rayo0Vertical")
         laser1 = atlas.findRegion("rayo1Vertical")
         laser2 = atlas.findRegion("rayo2Vertical")
         laser3 = atlas.findRegion("rayo3Vertical")
-        laserVertical = Animation<TextureRegion?>(0.1f, laser0, laser1, laser2, laser3)
+        laserVertical = Animation<TextureRegion>(0.1f, laser0, laser1, laser2, laser3)
 
         gas = atlas.findRegion("gas")
         star = atlas.findRegion("star")
@@ -211,19 +168,19 @@ object Assets {
         upgradeOn = atlas.findRegion("upgradeOn")
         upgradeOff = atlas.findRegion("upgradeOff")
 
-        mundos = LinkedHashMap<Int?, String?>()
-        mundos!!.put(0, "data/mundos/mundo00.tmx")
-        mundos!!.put(1, "data/mundos/mundo01.tmx")
-        mundos!!.put(2, "data/mundos/mundo02.tmx")
-        mundos!!.put(3, "data/mundos/mundo03.tmx")
-        mundos!!.put(4, "data/mundos/mundo04.tmx")
-        mundos!!.put(5, "data/mundos/mundo05.tmx")
-        mundos!!.put(6, "data/mundos/mundo06.tmx")
-        mundos!!.put(7, "data/mundos/mundo07.tmx")
-        mundos!!.put(8, "data/mundos/mundo08.tmx")
-        mundos!!.put(9, "data/mundos/mundo09.tmx")
-        mundos!!.put(10, "data/mundos/mundo10.tmx")
-        mundos!!.put(11, "data/mundos/mundo11.tmx")
+        mundos = LinkedHashMap()
+        mundos.put(0, "data/mundos/mundo00.tmx")
+        mundos.put(1, "data/mundos/mundo01.tmx")
+        mundos.put(2, "data/mundos/mundo02.tmx")
+        mundos.put(3, "data/mundos/mundo03.tmx")
+        mundos.put(4, "data/mundos/mundo04.tmx")
+        mundos.put(5, "data/mundos/mundo05.tmx")
+        mundos.put(6, "data/mundos/mundo06.tmx")
+        mundos.put(7, "data/mundos/mundo07.tmx")
+        mundos.put(8, "data/mundos/mundo08.tmx")
+        mundos.put(9, "data/mundos/mundo09.tmx")
+        mundos.put(10, "data/mundos/mundo10.tmx")
+        mundos.put(11, "data/mundos/mundo11.tmx")
 
         load(500)
     }
@@ -234,6 +191,6 @@ object Assets {
             map!!.dispose()
             map = null
         }
-        map = TmxMapLoader().load(mundos!!.get(numeroMundo))
+        map = TmxMapLoader().load(mundos.get(numeroMundo))
     }
 }
