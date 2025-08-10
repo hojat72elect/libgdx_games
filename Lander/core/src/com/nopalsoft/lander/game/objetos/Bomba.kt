c7002b3d1e5e358f5501a3fefffbee7a68a7f5b5
hojat72elect <hojat72elect@gmail.com>
2025-08-10 04:37:52 -0400

    Bomba.java -> kt

--- Lander/core/src/com/nopalsoft/lander/game/objetos/Bomba.kt
+++ Lander/core/src/com/nopalsoft/lander/game/objetos/Bomba.kt
@@ -1,65 +1,75 @@
-package com.nopalsoft.lander.game.objetos;
+package com.nopalsoft.lander.game.objetos
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
 
-public class Bomba {
+class Bomba(x: Float, y: Float, direccion: String) {
 
-    final static public float VELOCIDAD = 1f;
-    final public static int STATE_NORMAL = 0;
-    final public static int STATE_TOMADA = 1;
-    final public static int STATE_EXPLOSION = 2;
-    final public static float TIME_EXPLOSION = .05f * 20;
-    public static int DIRECCION_UP = 0;
-    public static int DIRECCION_RIGHT = 1;
-    final float TIME_TOMADA = .5f;
-    public Vector2 position;
-    public Vector2 velocidad;
+    @JvmField
+    var position: Vector2 = Vector2(x, y)
 
-    public float stateTime;
+    @JvmField
+    var velocidad: Vector2? = null
 
-    public int state;
-    public int direccion;
+    @JvmField
+    var stateTime: Float
 
-    public Bomba(float x, float y, String direccion) {
-        position = new Vector2(x, y);
-        stateTime = 0;
-        state = STATE_NORMAL;
+    @JvmField
+    var state: Int
+    var direccion: Int = 0
 
-        switch (direccion) {
-            case "arriba":
-                this.direccion = DIRECCION_UP;
-                velocidad = new Vector2(0, VELOCIDAD);
-                break;
-            case "abajo":
-                this.direccion = DIRECCION_UP;
-                velocidad = new Vector2(0, -VELOCIDAD);
-                break;
-            case "derecha":
-                this.direccion = DIRECCION_RIGHT;
-                velocidad = new Vector2(VELOCIDAD, 0);
-                break;
-            case "izquierda":
-                this.direccion = DIRECCION_RIGHT;
-                velocidad = new Vector2(-VELOCIDAD, 0);
-                break;
+    init {
+        stateTime = 0f
+        state = STATE_NORMAL
+
+        when (direccion) {
+            "arriba" -> {
+                this.direccion = DIRECCION_UP
+                velocidad = Vector2(0f, VELOCIDAD)
+            }
+
+            "abajo" -> {
+                this.direccion = DIRECCION_UP
+                velocidad = Vector2(0f, -VELOCIDAD)
+            }
+
+            "derecha" -> {
+                this.direccion = DIRECCION_RIGHT
+                velocidad = Vector2(VELOCIDAD, 0f)
+            }
+
+            "izquierda" -> {
+                this.direccion = DIRECCION_RIGHT
+                velocidad = Vector2(-VELOCIDAD, 0f)
+            }
         }
     }
 
-    public void update(float delta, Body body) {
-        position.x = body.getPosition().x;
-        position.y = body.getPosition().y;
+    fun update(delta: Float, body: Body) {
+        position.x = body.getPosition().x
+        position.y = body.getPosition().y
 
         if (state == STATE_TOMADA && stateTime > TIME_TOMADA) {
-            state = STATE_EXPLOSION;
-            stateTime = 0;
+            state = STATE_EXPLOSION
+            stateTime = 0f
         }
 
-        stateTime += delta;
+        stateTime += delta
     }
 
-    public void cambioDireccion() {
-        velocidad.x *= -1;
-        velocidad.y *= -1;
+    fun cambioDireccion() {
+        velocidad!!.x *= -1f
+        velocidad!!.y *= -1f
+    }
+
+    companion object {
+        private const val TIME_TOMADA: Float = .5f
+        private const val VELOCIDAD: Float = 1f
+        const val STATE_NORMAL: Int = 0
+        const val STATE_TOMADA: Int = 1
+        const val STATE_EXPLOSION: Int = 2
+        const val TIME_EXPLOSION: Float = .05f * 20
+        private const val DIRECCION_UP: Int = 0
+        private const val DIRECCION_RIGHT: Int = 1
     }
 }
