686a7bfe18a053b9deb1e97c7afc1f0fb03bea1a
hojat72elect <hojat72elect@gmail.com>
2025-08-13 02:49:11 -0400

    HeroForce.java -> kt HeroLumber.java -> kt

---
+++ Zombie_Wars/core/src/com/nopalsoft/zombiewars/objetos/HeroForce.kt
@@ -0,0 +1,18 @@
+package com.nopalsoft.zombiewars.objetos
+
+import com.badlogic.gdx.physics.box2d.Body
+import com.nopalsoft.zombiewars.Assets
+
+class HeroForce(body: Body) : Personajes(body) {
+    init {
+        DURATION_ATTACK = Assets.heroForceShoot.animationDuration
+        DURATION_DEAD = Assets.heroForceDie.animationDuration + .2f
+        VELOCIDAD_WALK = 1f
+        DAMAGE = 1
+        DISTANCE_ATTACK = 2f
+        TIME_TO_ATTACK_AGAIN = 2f
+        vidas = 5
+        isFacingLeft = false
+        tipo = TIPO_RANGO
+    }
+}
---
+++ Zombie_Wars/core/src/com/nopalsoft/zombiewars/objetos/HeroLumber.kt
@@ -0,0 +1,18 @@
+package com.nopalsoft.zombiewars.objetos
+
+import com.badlogic.gdx.physics.box2d.Body
+import com.nopalsoft.zombiewars.Assets
+
+class HeroLumber(body: Body) : Personajes(body) {
+    init {
+        DURATION_ATTACK = Assets.heroLumberShoot.animationDuration
+        DURATION_DEAD = Assets.heroForceDie.animationDuration + .2f
+        VELOCIDAD_WALK = 1f
+        DAMAGE = 1
+        DISTANCE_ATTACK = .5f
+        TIME_TO_ATTACK_AGAIN = 0f
+        vidas = 5
+        isFacingLeft = false
+        tipo = TIPO_NO_RANGO
+    }
+}
