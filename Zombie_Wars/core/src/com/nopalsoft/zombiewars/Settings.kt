57d471e2d2eb5c597a06fc3894486b0c35df2fb0
hojat72elect <hojat72elect@gmail.com>
2025-08-13 03:16:05 -0400

    Settings.java -> kt

---
+++ Zombie_Wars/core/src/com/nopalsoft/zombiewars/Settings.kt
@@ -0,0 +1,17 @@
+package com.nopalsoft.zombiewars
+
+object Settings {
+    @JvmField
+    var zoom = 1f // Zoom de la camara.
+
+    @JvmField
+    var isTest = true
+
+    @JvmStatic
+    fun load() {
+    }
+
+    @JvmStatic
+    fun save() {
+    }
+}
