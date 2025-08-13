2e9c427228feb16f1dcea20bc68bfb7db46fc454
hojat72elect <hojat72elect@gmail.com>
2025-08-13 02:44:21 -0400

    Zombie Wars : Migrated desktop module to kotlin

---
+++ Zombie_Wars/desktop/src/com/nopalsoft/zombiewars/desktop/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package com.nopalsoft.zombiewars.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.zombiewars.MainZombieWars
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        config.width = 800
+        config.height = 480
+        LwjglApplication(MainZombieWars(), config)
+    }
+}
