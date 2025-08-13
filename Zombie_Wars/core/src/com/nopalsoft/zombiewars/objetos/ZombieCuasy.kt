6aa45cd38b08cbb3ffb7a026ebe961c30d3de02d
hojat72elect <hojat72elect@gmail.com>
2025-08-13 02:55:33 -0400

    ZombieCuasy.java -> kt ZombieFrank.java -> kt ZombieKid.java -> kt ZombieMummy.java -> kt ZombiePan.java -> kt

---
+++ Zombie_Wars/core/src/com/nopalsoft/zombiewars/objetos/ZombieCuasy.kt
@@ -0,0 +1,17 @@
+package com.nopalsoft.zombiewars.objetos
+
+import com.badlogic.gdx.physics.box2d.Body
+import com.nopalsoft.zombiewars.Assets
+
+class ZombieCuasy(body: Body) : Personajes(body) {
+    init {
+        DURATION_ATTACK = Assets.zombieCuasyAttack.animationDuration
+        DURATION_DEAD = Assets.zombieCuasyDie.animationDuration + .2f
+        VELOCIDAD_WALK = .15f
+        DISTANCE_ATTACK = .35f
+        DAMAGE = 1
+        vidas = 5
+        isFacingLeft = true
+        tipo = TIPO_NO_RANGO
+    }
+}
