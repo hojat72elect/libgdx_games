211d6f4a7b6599c1254f552e3f5835538429fa84
hojat72elect <hojat72elect@gmail.com>
2025-08-23 22:18:45 -0400

    URacerRuntimeException.java -> kt

--- DriftGame/uracer-desktop/src/com/bitfire/uracer/utils/URacerRuntimeException.kt
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/utils/URacerRuntimeException.kt
@@ -1,12 +1,6 @@
-package com.bitfire.uracer.utils;
+package com.bitfire.uracer.utils
 
 /**
  * Typed runtime exception used throughout uracer
  */
-public class URacerRuntimeException extends RuntimeException {
-    private static final long serialVersionUID = 110779130456L;
-
-    public URacerRuntimeException(String message) {
-        super(message);
-    }
-}
+class URacerRuntimeException(message: String) : RuntimeException(message)
