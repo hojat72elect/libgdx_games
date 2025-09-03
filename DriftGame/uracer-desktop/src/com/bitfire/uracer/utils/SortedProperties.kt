3b2d1de9fd93eba0a76bcf39a9b500ad0e65da55
hojat72elect <hojat72elect@gmail.com>
2025-09-03 02:13:48 -0400

    SortedProperties.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/utils/SortedProperties.kt
@@ -0,0 +1,29 @@
+package com.bitfire.uracer.utils
+
+import java.util.Collections
+import java.util.Enumeration
+import java.util.Properties
+
+class SortedProperties : Properties() {
+    /**
+     * Overrides, called by the store method.
+     */
+    @Synchronized
+    override fun keys(): Enumeration<Any> {
+        val keysEnum = super.keys()
+        val keys = ArrayList<Any>()
+        while (keysEnum.hasMoreElements()) {
+            keys.add(keysEnum.nextElement())
+        }
+        keys.sortWith { o1: Any, o2: Any ->
+            val s1 = o1 as String
+            val s2 = o2 as String
+            s1.compareTo(s2, ignoreCase = true)
+        }
+        return Collections.enumeration(keys)
+    }
+
+    companion object {
+        private const val serialVersionUID = 2029498762824558018L
+    }
+}
