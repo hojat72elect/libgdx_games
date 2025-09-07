dd94d4b72309ac47eac9c3069f0ec04207a2c52a
hojat72elect <hojat72elect@gmail.com>
2025-09-07 01:31:53 -0400

    Box2DEntity.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/game/actors/Box2DEntity.kt
@@ -0,0 +1,52 @@
+package com.bitfire.uracer.game.actors
+
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.World
+import com.bitfire.uracer.entities.EntityRenderState
+import com.bitfire.uracer.utils.AMath
+
+abstract class Box2DEntity(@JvmField protected var box2dWorld: World) : SubframeInterpolableEntity() {
+
+    @JvmField
+    protected var body: Body? = null
+
+    override fun dispose() {
+        super.dispose()
+        box2dWorld.destroyBody(body)
+    }
+
+    fun getBody() = body!!
+
+    override fun saveStateTo(state: EntityRenderState) {
+        state.position.set(body!!.position)
+        state.orientation = body!!.angle
+    }
+
+    override fun isSubframeInterpolated() = true
+
+    override fun onBeforePhysicsSubstep() {
+        toNormalRelativeAngle()
+        super.onBeforePhysicsSubstep()
+    }
+
+    fun getWorldPosMt(): Vector2 = body!!.position
+
+    open fun setWorldPosMt(worldPosition: Vector2) {
+        body!!.setTransform(worldPosition, body!!.angle)
+        resetState()
+    }
+
+    fun getWorldOrientRads() = body!!.angle
+
+    open fun setWorldPosMt(worldPosition: Vector2, orientationRads: Float) {
+        body!!.setTransform(worldPosition, orientationRads)
+        resetState()
+    }
+
+    protected fun toNormalRelativeAngle() {
+        // normalize body angle since it can grow unbounded
+        val angle = AMath.normalRelativeAngle(body!!.angle)
+        body!!.setTransform(body!!.position, angle)
+    }
+}
