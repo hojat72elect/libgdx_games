6700106be9c5a10b3af64b03779dbbf99e0912b8
hojat72elect <hojat72elect@gmail.com>
2025-09-08 16:49:09 -0400

    docs

--- DriftGame/uracer-desktop/src/com/bitfire/postprocessing/filters/Convolve2D.java
+++ DriftGame/uracer-desktop/src/com/bitfire/postprocessing/filters/Convolve2D.java
@@ -3,32 +3,36 @@ package com.bitfire.postprocessing.filters;
 import com.bitfire.postprocessing.utils.PingPongBuffer;
 
 /**
- * Encapsulates a separable 2D convolution kernel filter
+ * Implements a separable 2D convolution filter.
+ * This filter is often used for effects like blurring, sharpening, or edge detection.
+ * <p>
+ * In here we have a two-pass rendering process; first a horizontal convolution is applied to the input image,
+ * then the result of the horizontal pass will be passed to the vertical convolution.
  */
 public final class Convolve2D extends MultipassFilter {
     public final int radius;
-    public final int length; // NxN taps filter, w/ N=length
+    public final int length;
 
     public final float[] weights, offsetsHor, offsetsVert;
 
-    private final Convolve1D hor;
-    private final Convolve1D vert;
+    private final Convolve1D horizontalPass;
+    private final Convolve1D verticalPass;
 
     public Convolve2D(int radius) {
         this.radius = radius;
         length = (radius * 2) + 1;
 
-        hor = new Convolve1D(length);
-        vert = new Convolve1D(length, hor.weights);
+        horizontalPass = new Convolve1D(length);
+        verticalPass = new Convolve1D(length, horizontalPass.weights);
 
-        weights = hor.weights;
-        offsetsHor = hor.offsets;
-        offsetsVert = vert.offsets;
+        weights = horizontalPass.weights;
+        offsetsHor = horizontalPass.offsets;
+        offsetsVert = verticalPass.offsets;
     }
 
     public void dispose() {
-        hor.dispose();
-        vert.dispose();
+        horizontalPass.dispose();
+        verticalPass.dispose();
     }
 
     public void upload() {
@@ -37,13 +41,13 @@ public final class Convolve2D extends MultipassFilter {
 
     @Override
     public void rebind() {
-        hor.rebind();
-        vert.rebind();
+        horizontalPass.rebind();
+        verticalPass.rebind();
     }
 
     @Override
     public void render(PingPongBuffer buffer) {
-        hor.setInput(buffer.capture()).render();
-        vert.setInput(buffer.capture()).render();
+        horizontalPass.setInput(buffer.capture()).render();
+        verticalPass.setInput(buffer.capture()).render();
     }
 }
