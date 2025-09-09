5175f8a64e43f4a48b8e4c6fcd3e8bf938ab8123
hojat72elect <hojat72elect@gmail.com>
2025-09-09 15:54:13 -0400

    ScaleUtils.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/utils/ScaleUtils.kt
@@ -0,0 +1,64 @@
+package com.bitfire.uracer.utils
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.math.Rectangle
+import com.bitfire.uracer.configuration.Config
+
+object ScaleUtils {
+
+    @JvmField
+    var PlayWidth = 0
+
+    @JvmField
+    var PlayHeight = 0
+
+    @JvmField
+    var CropX = 0
+
+    @JvmField
+    var CropY = 0
+
+    @JvmField
+    var PlayViewport: Rectangle? = null
+
+    @JvmField
+    var Scale = 0F
+
+    @JvmField
+    var RefAspect = 0F
+
+    @JvmStatic
+    fun initialize(displayWidth: Int, displayHeight: Int) {
+        val refW = Config.Graphics.ReferenceScreenWidth.toFloat()
+        val refH = Config.Graphics.ReferenceScreenHeight.toFloat()
+
+        // Maintain the aspect ratio by letterboxing.
+        RefAspect = refW / refH
+        val physicalWidth = displayWidth.toFloat()
+        val physicalHeight = displayHeight.toFloat()
+        val aspect = physicalWidth / physicalHeight
+        CropX = 0
+        CropY = 0
+
+        if (aspect > RefAspect) {
+            // Letterbox left and right
+            Scale = physicalHeight / refH
+            CropX = ((physicalWidth - refW * Scale) / 2F).toInt()
+        } else if (aspect < RefAspect) {
+
+            // Letterbox above and below
+            Scale = physicalWidth / refW
+            CropY = ((physicalHeight - refH * Scale) / 2f).toInt()
+        } else {
+            Scale = physicalWidth / refW
+        }
+
+        PlayWidth = (refW * Scale).toInt()
+        PlayHeight = (refH * Scale).toInt()
+        PlayViewport = Rectangle(CropX.toFloat(), CropY.toFloat(), PlayWidth.toFloat(), PlayHeight.toFloat())
+
+        Gdx.app.log("ScaleUtils", "Scale=$Scale")
+        Gdx.app.log("ScaleUtils", "Play=${PlayWidth}x${PlayHeight}")
+        Gdx.app.log("ScaleUtils", "Crop=${CropX}x${CropY}")
+    }
+}
