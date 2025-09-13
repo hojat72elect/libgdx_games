4430783a382f47820f99d2ee5a9d7cf8549868a4
hojat72elect <hojat72elect@gmail.com>
2025-09-12 20:32:50 -0400

    GameScreen.java -> kt

--- BricksGDX/core/src/com/salvador/bricks/screens/GameClass.kt
+++ BricksGDX/core/src/com/salvador/bricks/screens/GameClass.kt
@@ -1,13 +1,6 @@
-package com.salvador.bricks.screens;
+package com.salvador.bricks.screens
 
-import com.badlogic.gdx.Screen;
-import com.salvador.bricks.BrickBreaker;
+import com.badlogic.gdx.Screen
+import com.salvador.bricks.BrickBreaker
 
-public abstract class GameClass implements Screen {
-
-    public BrickBreaker game;
-
-    public GameClass(BrickBreaker game) {
-        this.game = game;
-    }
-}
+abstract class GameClass(@JvmField val game: BrickBreaker) : Screen
