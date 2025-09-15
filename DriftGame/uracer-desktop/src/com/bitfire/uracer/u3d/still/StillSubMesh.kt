9fd8b48a8ead602091de75f3d9f4df796afe53b8
hojat72elect <hojat72elect@gmail.com>
2025-09-14 22:09:06 -0400

    StillSubMesh.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/u3d/still/StillSubMesh.kt
@@ -0,0 +1,6 @@
+package com.bitfire.uracer.u3d.still
+
+import com.badlogic.gdx.graphics.Mesh
+import com.bitfire.uracer.u3d.model.SubMesh
+
+class StillSubMesh(name: String, mesh: Mesh, primitiveType: Int) : SubMesh(name, mesh, primitiveType)
