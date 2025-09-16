750847564396f9a61af4be9e9ddbd94f8a252595
hojat72elect <hojat72elect@gmail.com>
2025-09-15 21:01:06 -0400

    UStillModel.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/u3d/model/UStillModel.kt
@@ -0,0 +1,6 @@
+package com.bitfire.uracer.u3d.model
+
+import com.bitfire.uracer.u3d.still.StillModel
+import com.bitfire.uracer.u3d.still.StillSubMesh
+
+class UStillModel(subMeshes: Array<StillSubMesh>) : StillModel(*subMeshes)
