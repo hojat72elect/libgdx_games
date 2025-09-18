dbc2501deb5c6c3e5c907cec6baf448c20e35cb8
hojat72elect <hojat72elect@gmail.com>
2025-09-18 00:26:34 -0400

    GameLogicEvent.java -> kt

---
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/game/events/GameLogicEvent.kt
@@ -0,0 +1,31 @@
+package com.bitfire.uracer.game.events
+
+import com.bitfire.uracer.game.player.PlayerCar
+
+class GameLogicEvent : Event<GameLogicEvent.Type, GameLogicEvent.Order>(Type::class.java, Order::class.java) {
+
+    @JvmField
+    var player: PlayerCar? = null
+
+    /**
+     * defines the type of render queue.
+     */
+    enum class Type {
+        PlayerAdded,
+        PlayerRemoved,
+        GameRestart,
+        GameReset,
+        GameQuit
+    }
+
+    /**
+     * defines the position in the render queue specified by the Type parameter.
+     */
+    enum class Order {
+        MINUS_4, MINUS_3, MINUS_2, MINUS_1, DEFAULT, PLUS_1, PLUS_2, PLUS_3, PLUS_4
+    }
+
+    interface Listener : Event.Listener<Type, Order> {
+        override fun handle(source: Any, type: Type, order: Order)
+    }
+}
