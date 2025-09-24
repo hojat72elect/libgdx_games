569f543fc4b9ebb3b48efe2503a88b4765304a6c
hojat72elect <hojat72elect@gmail.com>
2025-09-24 19:13:30 -0400

    Cleaned up DebugStatistics.java

--- DriftGame/uracer-desktop/src/com/bitfire/uracer/game/debug/DebugStatistics.java
+++ DriftGame/uracer-desktop/src/com/bitfire/uracer/game/debug/DebugStatistics.java
@@ -13,66 +13,44 @@ import com.bitfire.uracer.configuration.PhysicsUtils;
 import com.bitfire.uracer.utils.AlgebraMath;
 
 public final class DebugStatistics {
-    // public statistical data
+
+    private final Pixmap pixels;
+    private final Texture texture;
+    private final TextureRegion region;
+    private final int PanelWidth;
+    private final int PanelHeight;
+    private final float ratio_rtime;
+    private final float ratio_ptime;
+    private final float ratio_fps;
+    private final long intervalNs;
+    private final float[] dataRenderTime;
+    private final float[] dataFps;
+    private final float[] dataPhysicsTime;
+    private final float[] dataTimeAliasing;
     public WindowedMean meanPhysics = new WindowedMean(16);
     public WindowedMean meanRender = new WindowedMean(16);
     public WindowedMean meanTickCount = new WindowedMean(16);
-
-    // internal data for graphics representation
-    private Pixmap pixels;
-    private Texture texture;
-    private TextureRegion region;
-    private int PanelWidth;
-    private int PanelHeight;
-    private float ratio_rtime, ratio_ptime, ratio_fps;
-
-    // internal timing data
     private long startTime;
-    private long intervalNs;
-
-    // internal stats data
-    private float[] dataRenderTime;
-    private float[] dataFps;
-    private float[] dataPhysicsTime;
-    private float[] dataTimeAliasing;
 
     public DebugStatistics(int width, int height, float updateHz) {
-        init(width, height, updateHz);
-    }
-
-    private void init(int width, int height, float updateHz) {
 
         final float oneOnUpdHz = 1f / updateHz;
 
         PanelWidth = width;
         PanelHeight = height;
         intervalNs = (long) (1000000000L * oneOnUpdHz);
-
         pixels = new Pixmap(PanelWidth, PanelHeight, Format.RGBA8888);
         texture = new Texture(width, height, Format.RGBA8888);
         texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
         region = new TextureRegion(texture, 0, 0, pixels.getWidth(), pixels.getHeight());
-
-        // create data
         dataRenderTime = new float[PanelWidth];
         dataFps = new float[PanelWidth];
         dataPhysicsTime = new float[PanelWidth];
         dataTimeAliasing = new float[PanelWidth];
-
-        // precompute constants
         ratio_rtime = ((float) PanelHeight / 2f) * PhysicsUtils.TimestepHz;
         ratio_ptime = ((float) PanelHeight / 2f) * PhysicsUtils.TimestepHz;
         ratio_fps = ((float) PanelHeight / 2f) * oneOnUpdHz;
 
-        reset();
-    }
-
-    public void dispose() {
-        pixels.dispose();
-        texture.dispose();
-    }
-
-    private void reset() {
         for (int i = 0; i < PanelWidth; i++) {
             dataRenderTime[i] = 0;
             dataPhysicsTime[i] = 0;
@@ -82,6 +60,11 @@ public final class DebugStatistics {
 
         plot();
         startTime = TimeUtils.nanoTime();
+    }
+
+    public void dispose() {
+        pixels.dispose();
+        texture.dispose();
     }
 
     public TextureRegion getRegion() {
@@ -103,7 +86,6 @@ public final class DebugStatistics {
     }
 
     private void plot() {
-        // background
         pixels.setColor(0, 0, 0, 0.8f);
         pixels.fill();
 
@@ -112,26 +94,22 @@ public final class DebugStatistics {
             int xc = PanelWidth - x - 1;
             int value;
 
-            // render time
             value = (int) (dataRenderTime[x] * ratio_rtime);
             if (value > 0) {
                 pixels.setColor(0, 0.5f, 1f, alpha);
                 pixels.drawLine(xc, 0, xc, value);
             }
 
-            // physics time
             value = (int) (dataPhysicsTime[x] * ratio_ptime);
             pixels.setColor(1, 1, 1, alpha);
             pixels.drawLine(xc, 0, xc, value);
 
-            // fps
             value = (int) (dataFps[x] * ratio_fps);
             if (value > 0) {
                 pixels.setColor(0, 1, 1, .8f);
                 pixels.drawPixel(xc, value);
             }
 
-            // time aliasing
             value = (int) (AlgebraMath.clamp(dataTimeAliasing[x] * PanelHeight, 0, PanelHeight));
             if (value > 0) {
                 pixels.setColor(1, 0, 1, .8f);
@@ -146,7 +124,6 @@ public final class DebugStatistics {
         long time = TimeUtils.nanoTime();
 
         if (time - startTime > intervalNs) {
-            // shift values
             for (int i = PanelWidth - 1; i > 0; i--) {
                 dataRenderTime[i] = dataRenderTime[i - 1];
                 dataPhysicsTime[i] = dataPhysicsTime[i - 1];
