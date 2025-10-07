cc56cf5646c37487bf0bde431fcde3b8125f71dd
hojat72elect <hojat72elect@gmail.com>
2025-10-07 17:27:26 -0400

    Changed the desktop default dimension to full-HD.

--- Shark_Adventure/lwjgl3/src/main/java/ca/hojat/shark_adventure/lwjgl3/Lwjgl3Launcher.java
+++ Shark_Adventure/lwjgl3/src/main/java/ca/hojat/shark_adventure/lwjgl3/Lwjgl3Launcher.java
@@ -28,7 +28,7 @@ public class Lwjgl3Launcher {
          */
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
 
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(1920, 1080);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
