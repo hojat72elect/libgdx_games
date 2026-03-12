bc4723b5fde4e33e71f6834f6a2d067ea3e258fe
hojat72elect <hojat72elect@gmail.com>
2026-03-12 19:23:29 -0400

    Fixed an issue.

--- Dont_Tap/desktop/src/com/nopalsoft/donttap/desktop/DesktopLauncher.kt
+++ Dont_Tap/desktop/src/com/nopalsoft/donttap/desktop/DesktopLauncher.kt
@@ -3,12 +3,13 @@ package com.nopalsoft.donttap.desktop
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.nopalsoft.donttap.DoNotTapGame
+import com.nopalsoft.donttap.handlers.FloatFormatter
 
 fun main() {
+    val formatter = FloatFormatter { format, number -> String.format(format, number) }
     val config = Lwjgl3ApplicationConfiguration()
-    config.setWindowedMode(800, 480)
-    config.setTitle("Ninja Runner")
-    val game = DoNotTapGame(null)
-    Lwjgl3Application(game, config)
+    config.setTitle("Do not tap the wrong tile")
+    config.setWindowedMode(480, 800)
+    Lwjgl3Application(DoNotTapGame(formatter), config)
 }
 
