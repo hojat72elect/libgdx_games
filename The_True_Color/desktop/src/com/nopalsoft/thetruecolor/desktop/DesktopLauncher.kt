3b54abdba24373f47862604d35465aad45039563
hojat72elect <hojat72elect@gmail.com>
2026-03-12 23:18:33 -0400

    Fixed an issue.

--- The_True_Color/desktop/src/com/nopalsoft/thetruecolor/desktop/DesktopLauncher.kt
+++ The_True_Color/desktop/src/com/nopalsoft/thetruecolor/desktop/DesktopLauncher.kt
@@ -2,12 +2,11 @@ package com.nopalsoft.thetruecolor.desktop
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
-import com.nopalsoft.thetruecolor.MainStreet
+import com.nopalsoft.thetruecolor.TrueColorGame
 
 fun main() {
     val config = Lwjgl3ApplicationConfiguration()
-    config.setTitle("StreetSwipinRace")
     config.setWindowedMode(480, 800)
-    Lwjgl3Application(MainStreet(), config)
+    Lwjgl3Application(TrueColorGame(), config)
 }
 
